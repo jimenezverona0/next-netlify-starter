b9388cb655b8a3bf04f2c072c254b1cfffcbc627
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-16 03:08:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -93,23 +93,7 @@ export const getStaticProps = async () => {
   const statusCode2 = response2.status;
   const data2 = await response2.text();
 
-    var bankValue = data2.substring(
-      data2.indexOf(bank.toUpperCase()) - ('<option value="XXXX">XXXXXXXX XXXXXXXXXXX XXXXXXXXXX</option>'.length),
-      data2.indexOf(bank.toUpperCase())
-    ).substring(
-      data2.substring(
-        data2.indexOf(bank.toUpperCase()) - ('<option value="XXXX">XXXXXXXX XXXXXXXXXXX XXXXXXXXXX</option>'.length),
-        data2.indexOf(bank.toUpperCase())
-      ).indexOf('<option value="') + '<option value="'.length,
-      data2.substring(
-        data2.indexOf(bank.toUpperCase()) - ('<option value="XXXX">XXXXXXXX XXXXXXXXXXX XXXXXXXXXX</option>'.length),
-        data2.indexOf(bank.toUpperCase())
-      ).indexOf('"', data2.substring(
-        data2.indexOf(bank.toUpperCase()) - ('<option value="XXXX">XXXXXXXX XXXXXXXXXXX XXXXXXXXXX</option>'.length),
-        data2.indexOf(bank.toUpperCase())
-      ).indexOf('<option value="') + '<option value="'.length
-    ));
-
+    const bankValue = data2.substring(data2.indexOf(bank.toUpperCase()) - '<option value="XXXX">XXXXXXXX XXXXXXXXXXX XXXXXXXXXX</option>'.length, data2.indexOf(bank.toUpperCase())).substring(data2.indexOf('<option value="') + '<option value="'.length, data2.indexOf('"', data2.indexOf('<option value="') + '<option value="'.length));
     const paymentID = data2.substring(data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length, data2.indexOf('/', data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length));
     const urlPost = 'https://secure.payzen.lat/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/' + paymentID + '/submit';
     const idNumber = numgenerator();
