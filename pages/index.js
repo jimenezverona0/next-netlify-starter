1223d99d240cfd39d21be0393e2fbb585182c043
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-17 18:06:28 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { useState } from 'react';
+import Head from 'next/head'
 
 function namegenerator() {
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
