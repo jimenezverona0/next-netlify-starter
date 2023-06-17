fd114c3bf3a5cb8ecca3330d6adcb30d9cb2f6d2
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-17 17:08:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,199 +1,91 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import useState from 'react'
+import useForm from 'react-hook-form'
 
-function namegenerator() {
-    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const CreateDraft = () => {
+    const [data, setData] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    
+    const getData = async (d) => {
+        const variantID = d.variantID;
+        setData('');
+        setError('');
+        setLoading(true);
 
-    var namebase = lowercase;
-    var namelength = 15;
+        const method = 'POST';
 
-    var samplename = getRandomSample(namebase, namelength);
-    var name = samplename.join('');
-    return name;
-}
+        let postBody = "vads_action_mode=INTERACTIVE&vads_amount=5000000&vads_ctx_mode=PRODUCTION&vads_currency=170&vads_cust_cell_phone=3107538520&vads_payment_cards=PSE&vads_cust_email=gcsxsjdgsnbndj%40karenkey.com&vads_cust_first_name=nbdoskjbmoidjgiod+&vads_cust_last_name=dskfoisdjgoig&vads_language=es&vads_cust_phone=&vads_page_action=PAYMENT&vads_payment_config=SINGLE&vads_site_id=23824163&vads_trans_date=20230617205901&vads_trans_id=08681b&vads_version=V2&signature=pQpcmzSfjdxGFh3m%2FXkMlxsWFXoaZUTzdUumZlJMo5M%3D&pagar=";
 
-function numgenerator() {
-    var nums = '0123456789';
-
-    var numbase = nums;
-    var numlength = 8;
-
-    var samplenum = getRandomSample(numbase, numlength);
-    var halfnum = samplenum.join('');
-    var num = "31" + halfnum;
-    return num;
-}
-
-function getRandomSample(base, length) {
-    var sample = [];
-    while (sample.length < length) {
-        var randomIndex = Math.floor(Math.random() * base.length);
-        var randomElement = base.charAt(randomIndex);
-        if (!sample.includes(randomElement)) {
-            sample.push(randomElement);
+        const options = {
+            method: method,
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "cross-site",
+                "Sec-Fetch-User": "?1"
+            },
+            body: postBody
         }
-    }
-    return sample;
-}
 
-export const getStaticProps = async () => {
+        const url = "https://secure.payzen.lat/vads-payment/"
 
-  var cellphone = numgenerator()
-  var email = namegenerator() + "@gmail.com"
-  var firstName = namegenerator()
-  var lastName = namegenerator()
-  var amount = "50000"
-    var bank = "nequi"
-  
-  const response1 = await fetch("https://api.soyveci.com/transactions/smartlink/v2", {
-    "credentials": "omit",
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-        "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Content-Type": "application/json;charset=utf-8",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-site"
-    },
-    "referrer": "https://soyveci.com/link/?phone=3207625047",
-    "body": "{\"transaction\":{\"payment_type\":2,\"origin_name\":\"" + firstName + "\",\"origin_lastname\":\"" + lastName + "\",\"origin_document\":\"\",\"origin_phone\":\"" + cellphone + "\",\"origin_email\":\"" + email + "\",\"destination_phone\":\"3207625047\",\"amount\":\"" + amount + "\",\"supplier_code\":\"f0bd118b78388b4e4a02b40260c33a04d12a9ce314a3c4538de3cca294b25aa8\"}}",
-    "method": "POST",
-    "mode": "cors"
-});
-  const statusCode = response1.status;
-  const data1 = await response1.json();
-  const transDate = data1['data']['date']
-  const transID = data1['data']['code']
-  const signature = data1['data']['signature_pse']
+        const res = await fetch(url, options);
 
-  console.log(transDate, transID, signature, statusCode);
-  
-  const response2 = await fetch("https://secure.payzen.lat/vads-payment/", {
-    "credentials": "include",
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache"
-    },
-    "referrer": "https://soyveci.com/link/?phone=3207625047",
-    "body": "vads_action_mode=INTERACTIVE&vads_amount=" + amount + "00&vads_ctx_mode=PRODUCTION&vads_currency=170&vads_cust_cell_phone=" + cellphone + "&vads_payment_cards=PSE&vads_cust_email=" + email + "&vads_cust_first_name=" + firstName + "&vads_cust_last_name=" + lastName + "&vads_language=es&vads_cust_phone=&vads_page_action=PAYMENT&vads_payment_config=SINGLE&vads_site_id=23824163&vads_trans_date=" + transDate + "&vads_trans_id=" + transID + "&vads_version=V2&signature="+ signature +"&pagar=",
-    "method": "POST",
-    "mode": "cors"
-});
-  const statusCode2 = response2.status;
-  const data2 = await response2.text();
+        const final = await res.json();
 
-    const regex = new RegExp(`<option value="([^"]*)">.*${bank.toUpperCase()}.*</option>`);
-    const match = data2.match(regex);
-    const bankValue = match[1];
-    const paymentID = data2.substring(data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length, data2.indexOf('/', data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length));
-    const urlPost = 'https://secure.payzen.lat/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/' + paymentID + '/submit';
-    const idNumber = numgenerator();
+        console.log('final data: ', final);
 
-  console.log(data2, statusCode2);
+        if (final.error) {
+            setError(final.error);
+            setLoading(false);
+        } else {
+            setData(final.success);
+            setLoading(false);
+        }
 
-    const response3 = await fetch(urlPost, {
-        "credentials": "include",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-            "Accept": "text/plain, */*; q=0.01",
-            "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Referer": "https://soyveci.com/",
-            "Origin": "https://soyveci.com"
-        },
-        "body": "bank=" + bankValue + "&person_type=PHYSICAL&proof_of_id_type=CITIZENSHIP_CARD&proof_of_id_number=" + idNumber + "&person_name=" + firstName + "+" + lastName + "&phone_number=" + cellphone + "&email=" + email + "&raw_locale=es",
-        "method": "POST",
-        "mode": "cors"
-    });
-    
-    const statusCode3 = response3.status;
-    const data3 = await response3.text();
-    let enc = data3.substring(data3.indexOf('?enc=') + '?enc='.length, data3.length);
-    const idNumber2 = numgenerator()
-    const firstName2 = namegenerator()
-    const lastName2 = namegenerator()
-    const cellphone2 = numgenerator()
-    const email2 = namegenerator() + "@gmail.com"
-
-    const response4 = await fetch("https://registro.pse.com.co/PSEUserRegister/api/GetPreferences", {
-        "credentials": "include",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "host":"registro.pse.com.co"
-        },
-        "method": "GET",
-        "mode": "cors"
-    });
-
-    const setCookieHeader = response4.headers.get('set-cookie');
-    const cookies = parseCookies(setCookieHeader);
-    
-    function parseCookies(cookieHeader) {
-      if (!cookieHeader) {
-        return [];
-      }
-    
-      const cookieList = cookieHeader.split(';');
-      const cookies1 = cookieList.map(cookie => cookie.trim());
-    
-      return cookies1;
+        reset({})
     }
 
-    var miCookie = cookies[13];
+    return (
+        <div>
+            <div className="container text-start">
+                {loading && <div className="text-center mt-5">...Loading</div>}
+                {error && !loading && <h4 className="text-danger text-center mt-5">Error: {error}</h4>}
+                {data && !loading && !error && <h5 className="mt-5 text-success text-center">Success</h5>}
+                {data && !loading && !error && <div className="mt-1 mb-2 text-center"><span className="fw-bold">Your result:</span> {data}</div>}
+                {!data && !error && !loading && <h5 className="my-4">Create draft order: </h5>}
+                {data && !error && !loading && <h5 className="my-4">Create another: </h5>}
+                {error && !loading && <h5 className="mb-2 mt-4">Try again: </h5>}
+                {!loading && <div>
+                    <form onSubmit = {handleSubmit(getData)}>
+                        <label className="form-ckeck-label me-3">Enter Variant ID:</label>
+                        <input className="border border-2 border-light rounded-3"
+                            type="text"
+                            name="variantID"
+                            placeholder="i.e 1234567890"
+                            {...register("variantID", {
+                                required: 'Variant ID is required',
+                                pattern: { value: /^[0-9]*$/, message: "Please enter numbers only." }
+                            })}
+                        />
 
-    const response5 = await fetch("https://registro.pse.com.co/PSEUserRegister/api/GetPreferences", {
-        "credentials": "include",
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type": "application/json; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "host":"registro.pse.com.co",
-        },
-        "body": {"page":"create","enc":""},
-        "method": "POST",
-        "mode": "cors"
-    });
-
-    const statusCode5 = response5.status;
-    const data5 = await response5.text();
-    
-  return {
-    props: {cellphone: cellphone}
-  }
+                        <button className="ms-3 bg-success text-white border-0 rounded-3">Create</button>
+                        {errors.variantID && <div className="formError text-danger mb-3">{errors.variantID.message}</div>}
+                    </form>
+                </div>}
+            </div>
+        </div>
+    );
 }
 
-const Home = ({cellphone}) => {
-  console.log(cellphone);
-  return (
-    <>
-    </>
-  );
-}
-
-export default Home;
+export default CreateDraft;
+                
