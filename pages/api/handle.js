f750ceb9e3357cf081857bdc7081d05febfa8c4b
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-18 00:48:14 -0500

    Update handle.js

--- pages/api/handle.js
+++ pages/api/handle.js
@@ -1,5 +1,7 @@
 const handler = async (req, res) => {
 
+  const variantID = req.body.variantID;
+
   const method = 'POST';
 
   let postBody = "vads_action_mode=INTERACTIVE&vads_amount=5000000&vads_ctx_mode=PRODUCTION&vads_currency=170&vads_cust_cell_phone=3014353213&vads_payment_cards=PSE&vads_cust_email=cignugaltu%40gufum.com&vads_cust_first_name=FHSGSGDGDS&vads_cust_last_name=DGSDGDGS&vads_language=es&vads_cust_phone=&vads_page_action=PAYMENT&vads_payment_config=SINGLE&vads_site_id=23824163&vads_trans_date=20230618051445&vads_trans_id=4a7fb2&vads_version=V2&signature=7UDSovtLxOsRkHU0mnh5YsW4d5guxvFVUSLt%2FSOGgF8%3D&pagar=";
@@ -21,4 +23,11 @@ const handler = async (req, res) => {
   }
 
   const url = "https://secure.payzen.lat/vads-payment/";
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return res.end(JSON.stringify({'result': data}));
 }
+
+export default handler; 
