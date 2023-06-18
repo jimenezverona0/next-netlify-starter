31258284274bdc48e950ceec36c729d97a246d64
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-18 01:59:59 -0500

    Update handle.js

--- pages/api/handle.js
+++ pages/api/handle.js
@@ -66,8 +66,32 @@ const handler = async (req, res) => {
   const transDate = data1['data']['date']
   const transID = data1['data']['code']
   const signature = data1['data']['signature_pse']
+  
+  const response2 = await fetch("https://secure.payzen.lat/vads-payment/", {
+      "credentials": "include",
+      "headers": {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1",
+          "Pragma": "no-cache",
+          "Cache-Control": "no-cache"
+      },
+      "referrer": "https://soyveci.com/link/?phone=3207625047",
+      "body": "vads_action_mode=INTERACTIVE&vads_amount=" + amount + "00&vads_ctx_mode=PRODUCTION&vads_currency=170&vads_cust_cell_phone=" + cellphone + "&vads_payment_cards=PSE&vads_cust_email=" + email + "&vads_cust_first_name=" + firstName + "&vads_cust_last_name=" + lastName + "&vads_language=es&vads_cust_phone=&vads_page_action=PAYMENT&vads_payment_config=SINGLE&vads_site_id=23824163&vads_trans_date=" + transDate + "&vads_trans_id=" + transID + "&vads_version=V2&signature="+ signature +"&pagar=",
+      "method": "POST",
+      "mode": "cors"
+  });
+  
+  const statusCode2 = response2.status;
+  const data2 = await response2.text();
 
-  return res.end(JSON.stringify({'result': data1}));
+  return res.end(JSON.stringify({'result': data2}));
 }
 
 export default handler;
