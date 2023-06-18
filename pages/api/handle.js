60bdcb61e85dfcaf853d5c7cf93c81886461b1ad
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-06-18 04:34:11 -0500

    Update handle.js

--- pages/api/handle.js
+++ pages/api/handle.js
@@ -87,11 +87,18 @@ const handler = async (req, res) => {
       "method": "POST",
       "mode": "cors"
   });
-  
+
   const statusCode2 = response2.status;
   const data2 = await response2.text();
+  let paymentID = data2.substring(
+      data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length,
+      data2.indexOf('/', data2.indexOf('<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/') + '<input type="hidden" name="submit_url" value="https://secure.payzen.lat:443/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/'.length)
+    );
 
-  return res.end(JSON.stringify({'data1': data1, 'data2': data2}));
+  let urlPost = 'https://secure.payzen.lat/checkout/v3/web/PSE-a729d3a0-5d33-4c28-8acd-393f4fd4ee33/webpayments/' + paymentID + '/submit';
+  let idNumber = numgenerator();
+
+  return res.end(JSON.stringify({'paymentID': paymentID, 'urlPost': urlPost, 'idNumber': idNumber}));
 }
 
 export default handler;
