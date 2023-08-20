20b2ce68f6f5b87fc78f8310c80132844ae5fc8e
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-08-20 14:14:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -76,118 +76,124 @@ function Home() {
 
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
           <Head>
             {/* Resto del código... */}
           </Head>
           <Header />
-          {/* VECI... */}
-          <div style={{ marginBottom: "20px" }}>
-              <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Ingresa un valor"
-              />
-              <br /><br />
-              <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
-                <option value="0">Selecciona una opción</option>
-                <option value="1558">BAN100</option>
-                <option value="1059">BANCAMIA S.A.</option>
-                <option value="1040">BANCO AGRARIO</option>
-                <option value="1052">BANCO AV VILLAS</option>
-                <option value="1013">BANCO BBVA COLOMBIA S.A.</option>
-                <option value="1032">BANCO CAJA SOCIAL</option>
-                <option value="1066">BANCO COOPERATIVO COOPCENTRAL</option>
-                <option value="1051">BANCO DAVIVIENDA</option>
-                <option value="1001">BANCO DE BOGOTA</option>
-                <option value="1023">BANCO DE OCCIDENTE</option>
-                <option value="1062">BANCO FALABELLA </option>
-                <option value="1063">BANCO FINANDINA S.A. BIC</option>
-                <option value="1012">BANCO GNB SUDAMERIS</option>
-                <option value="1006">BANCO ITAU</option>
-                <option value="1060">BANCO PICHINCHA S.A.</option>
-                <option value="1002">BANCO POPULAR</option>
-                <option value="1065">BANCO SANTANDER COLOMBIA</option>
-                <option value="1069">BANCO SERFINANZA</option>
-                <option value="1303">BANCO UNION antes GIROS</option>
-                <option value="1007">BANCOLOMBIA</option>
-                <option value="1061">BANCOOMEVA S.A.</option>
-                <option value="1283">CFA COOPERATIVA FINANCIERA</option>
-                <option value="1009">CITIBANK </option>
-                <option value="1370">COLTEFINANCIERA</option>
-                <option value="1292">CONFIAR COOPERATIVA FINANCIERA</option>
-                <option value="1291">COOFINEP COOPERATIVA FINANCIERA</option>
-                <option value="1289">COTRAFA</option>
-                <option value="1097">DALE</option>
-                <option value="1551">DAVIPLATA</option>
-                <option value="1637">IRIS</option>
-                <option value="1070">LULO BANK</option>
-                <option value="1801">MOVII S.A.</option>
-                <option value="1507">NEQUI</option>
-                <option value="1811">RAPPIPAY</option>
-                <option value="1019">SCOTIABANK COLPATRIA</option>
-              </select>
-              <br /><br />
-              <button onClick={handleClick}>Generar enlace</button>
-              <br /><br />
-              <p1 id="link">[Aquí aparecerá tu enlace]</p1>
-              <br /><br />
-              <button onClick={handleCopy}>Copiar</button>
-          </div>
-          {/* BITSO... */}
-          <div style={{ marginBottom: "20px" }}>
-              <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Ingresa un valor"
-              />
-              <br /><br />
-              <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
-                <option value="0">Selecciona una opción</option>
-                <option value="1558">BAN100</option>
-                <option value="1059">BANCAMIA S.A.</option>
-                <option value="1040">BANCO AGRARIO</option>
-                <option value="1052">BANCO AV VILLAS</option>
-                <option value="1013">BANCO BBVA COLOMBIA S.A.</option>
-                <option value="1032">BANCO CAJA SOCIAL</option>
-                <option value="1066">BANCO COOPERATIVO COOPCENTRAL</option>
-                <option value="1051">BANCO DAVIVIENDA</option>
-                <option value="1001">BANCO DE BOGOTA</option>
-                <option value="1023">BANCO DE OCCIDENTE</option>
-                <option value="1062">BANCO FALABELLA </option>
-                <option value="1063">BANCO FINANDINA S.A. BIC</option>
-                <option value="1012">BANCO GNB SUDAMERIS</option>
-                <option value="1006">BANCO ITAU</option>
-                <option value="1060">BANCO PICHINCHA S.A.</option>
-                <option value="1002">BANCO POPULAR</option>
-                <option value="1065">BANCO SANTANDER COLOMBIA</option>
-                <option value="1069">BANCO SERFINANZA</option>
-                <option value="1303">BANCO UNION antes GIROS</option>
-                <option value="1007">BANCOLOMBIA</option>
-                <option value="1061">BANCOOMEVA S.A.</option>
-                <option value="1283">CFA COOPERATIVA FINANCIERA</option>
-                <option value="1009">CITIBANK </option>
-                <option value="1370">COLTEFINANCIERA</option>
-                <option value="1292">CONFIAR COOPERATIVA FINANCIERA</option>
-                <option value="1291">COOFINEP COOPERATIVA FINANCIERA</option>
-                <option value="1289">COTRAFA</option>
-                <option value="1097">DALE</option>
-                <option value="1551">DAVIPLATA</option>
-                <option value="1637">IRIS</option>
-                <option value="1070">LULO BANK</option>
-                <option value="1801">MOVII S.A.</option>
-                <option value="1507">NEQUI</option>
-                <option value="1811">RAPPIPAY</option>
-                <option value="1019">SCOTIABANK COLPATRIA</option>
-              </select>
-              <br /><br />
-              <button onClick={handleClick}>Generar enlace</button>
-              <br /><br />
-              <p1 id="link">[Aquí aparecerá tu enlace]</p1>
-              <br /><br />
-              <button onClick={handleCopy}>Copiar</button>
+          <div style={{ display: "flex" }}>
+              {/* VECI... */}
+              <div style={{ marginRight: "20px" }}>
+                  <div style={{ marginBottom: "20px" }}>
+                      <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        placeholder="Ingresa un valor"
+                      />
+                      <br /><br />
+                      <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+                        <option value="0">Selecciona una opción</option>
+                        <option value="1558">BAN100</option>
+                        <option value="1059">BANCAMIA S.A.</option>
+                        <option value="1040">BANCO AGRARIO</option>
+                        <option value="1052">BANCO AV VILLAS</option>
+                        <option value="1013">BANCO BBVA COLOMBIA S.A.</option>
+                        <option value="1032">BANCO CAJA SOCIAL</option>
+                        <option value="1066">BANCO COOPERATIVO COOPCENTRAL</option>
+                        <option value="1051">BANCO DAVIVIENDA</option>
+                        <option value="1001">BANCO DE BOGOTA</option>
+                        <option value="1023">BANCO DE OCCIDENTE</option>
+                        <option value="1062">BANCO FALABELLA </option>
+                        <option value="1063">BANCO FINANDINA S.A. BIC</option>
+                        <option value="1012">BANCO GNB SUDAMERIS</option>
+                        <option value="1006">BANCO ITAU</option>
+                        <option value="1060">BANCO PICHINCHA S.A.</option>
+                        <option value="1002">BANCO POPULAR</option>
+                        <option value="1065">BANCO SANTANDER COLOMBIA</option>
+                        <option value="1069">BANCO SERFINANZA</option>
+                        <option value="1303">BANCO UNION antes GIROS</option>
+                        <option value="1007">BANCOLOMBIA</option>
+                        <option value="1061">BANCOOMEVA S.A.</option>
+                        <option value="1283">CFA COOPERATIVA FINANCIERA</option>
+                        <option value="1009">CITIBANK </option>
+                        <option value="1370">COLTEFINANCIERA</option>
+                        <option value="1292">CONFIAR COOPERATIVA FINANCIERA</option>
+                        <option value="1291">COOFINEP COOPERATIVA FINANCIERA</option>
+                        <option value="1289">COTRAFA</option>
+                        <option value="1097">DALE</option>
+                        <option value="1551">DAVIPLATA</option>
+                        <option value="1637">IRIS</option>
+                        <option value="1070">LULO BANK</option>
+                        <option value="1801">MOVII S.A.</option>
+                        <option value="1507">NEQUI</option>
+                        <option value="1811">RAPPIPAY</option>
+                        <option value="1019">SCOTIABANK COLPATRIA</option>
+                      </select>
+                      <br /><br />
+                      <button onClick={handleClick}>Generar enlace</button>
+                      <br /><br />
+                      <p1 id="link">[Aquí aparecerá tu enlace]</p1>
+                      <br /><br />
+                      <button onClick={handleCopy}>Copiar</button>
+                  </div>
+              </div>
+              {/* BITSO... */}
+              <div>
+                  <div style={{ marginBottom: "20px" }}>
+                      <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        placeholder="Ingresa un valor"
+                      />
+                      <br /><br />
+                      <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+                        <option value="0">Selecciona una opción</option>
+                        <option value="1558">BAN100</option>
+                        <option value="1059">BANCAMIA S.A.</option>
+                        <option value="1040">BANCO AGRARIO</option>
+                        <option value="1052">BANCO AV VILLAS</option>
+                        <option value="1013">BANCO BBVA COLOMBIA S.A.</option>
+                        <option value="1032">BANCO CAJA SOCIAL</option>
+                        <option value="1066">BANCO COOPERATIVO COOPCENTRAL</option>
+                        <option value="1051">BANCO DAVIVIENDA</option>
+                        <option value="1001">BANCO DE BOGOTA</option>
+                        <option value="1023">BANCO DE OCCIDENTE</option>
+                        <option value="1062">BANCO FALABELLA </option>
+                        <option value="1063">BANCO FINANDINA S.A. BIC</option>
+                        <option value="1012">BANCO GNB SUDAMERIS</option>
+                        <option value="1006">BANCO ITAU</option>
+                        <option value="1060">BANCO PICHINCHA S.A.</option>
+                        <option value="1002">BANCO POPULAR</option>
+                        <option value="1065">BANCO SANTANDER COLOMBIA</option>
+                        <option value="1069">BANCO SERFINANZA</option>
+                        <option value="1303">BANCO UNION antes GIROS</option>
+                        <option value="1007">BANCOLOMBIA</option>
+                        <option value="1061">BANCOOMEVA S.A.</option>
+                        <option value="1283">CFA COOPERATIVA FINANCIERA</option>
+                        <option value="1009">CITIBANK </option>
+                        <option value="1370">COLTEFINANCIERA</option>
+                        <option value="1292">CONFIAR COOPERATIVA FINANCIERA</option>
+                        <option value="1291">COOFINEP COOPERATIVA FINANCIERA</option>
+                        <option value="1289">COTRAFA</option>
+                        <option value="1097">DALE</option>
+                        <option value="1551">DAVIPLATA</option>
+                        <option value="1637">IRIS</option>
+                        <option value="1070">LULO BANK</option>
+                        <option value="1801">MOVII S.A.</option>
+                        <option value="1507">NEQUI</option>
+                        <option value="1811">RAPPIPAY</option>
+                        <option value="1019">SCOTIABANK COLPATRIA</option>
+                      </select>
+                      <br /><br />
+                      <button onClick={handleClick}>Generar enlace</button>
+                      <br /><br />
+                      <p1 id="link">[Aquí aparecerá tu enlace]</p1>
+                      <br /><br />
+                      <button onClick={handleCopy}>Copiar</button>
+                  </div>
+              </div>
           </div>
       </div>
     </>
