cd33259f5081449dab5d9a448391161a1e364a0f
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-08-21 17:03:00 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,10 @@ function getRandomSample(base, length) {
     return sample;
 }
 
-function buildAuthHeader(httpMethod, url) {
-    
+function buildAuthHeader(amount) {
+
+    const httpMethod = 'GET';
+    const url = 'https://bitso.com/api/v3/funding_references?currency=cop&network=pse&protocol=pse&asset=cop&amount=' + amount;
     const jsonPayload = '';
     const key = "rUjoHeypjS";
     const secret = "03c7eb7284738312de8c4fc7509ab65d";
@@ -50,30 +52,21 @@ function buildAuthHeader(httpMethod, url) {
     }
     const nonce = Date.now();
     const msgConcat = nonce + httpMethod.toUpperCase() + requestPath + jsonPayload;
-    const hmac = require('crypto').createHmac('sha256', utf8ToBuffer(secret));
-    hmac.update(utf8ToBuffer(msgConcat));
-    const signature = hmac.digest('hex');
+    const signature = require('crypto').createHmac('sha256', secret)
+                            .update(msgConcat)
+                            .digest('hex');
     return {
-        'Authorization': `Bitso ${key}:${nonce}:${signature}`
+        `Bitso ${key}:${nonce}:${signature}`
     };
-}
-
-function utf8ToBuffer(str) {
-    const buffer = new ArrayBuffer(str.length);
-    const view = new Uint8Array(buffer);
-    for (let i = 0; i < str.length; i++) {
-        view[i] = str.charCodeAt(i);
-    }
-    return buffer;
 }
 
 function Home() {
   const [inputValue, setInputValue] = useState('');
   const [selectedOption, setSelectedOption] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
-  const [selectedOption2, setSelectedOption2] = useState('');
 
   const handleClick = async () => {
+    const authHeader = buildAuthHeader(inputValue);
+    console.log(inputValue)
     // Aquí puedes usar el valor ingresado por el usuario (inputValue) como desees
     console.log('Valor ingresado:', inputValue);
     console.log('Banco seleccionado:', selectedOption);
@@ -101,41 +94,8 @@ function Home() {
     console.log(final);
   };
 
-  const handleClick2 = async () => {
-
-    const httpMethod = 'GET';
-    const url2 = 'https://bitso.com/api/v3/funding_references?currency=cop&network=pse&protocol=pse&asset=cop&amount=' + inputValue2;
-    const authHeader = buildAuthHeader(httpMethod, url2);
-    console.log(authHeader);
-    // Aquí puedes usar el valor ingresado por el usuario (inputValue) como desees
-    console.log('Valor ingresado:', inputValue2);
-    console.log('Banco seleccionado:', selectedOption2);
-    // Llama a la función que deseas ejecutar con el valor ingresado
-    // ...
-    const method = 'POST';
-
-    const options = {
-        method: method,
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({'amount':inputValue2, 'bank': selectedOption2})
-    }
-
-    const url = '/api/handle2';
-
-    const res = await fetch(url, options);
-
-    const final = await res.json();
-
-    // Ejemplo de actualización del estado
-    document.getElementById('link2').innerText = final.link;
-
-    console.log(final);
-  };
-
   const handleCopy = () => {
-    navigator.clipboard.writeText(document.getElementById('link2').innerText);
+    navigator.clipboard.writeText(document.getElementById('link').innerText);
   };
 
   return (
@@ -199,63 +159,6 @@ function Home() {
                       <button onClick={handleClick}>Generar enlace</button>
                       <br /><br />
                       <p1 id="link">[Aquí aparecerá tu enlace]</p1>
-                      <br /><br />
-                      <button onClick={handleCopy}>Copiar</button>
-                  </div>
-              </div>
-              {/* BITSO... */}
-              <div>
-                  <div style={{ marginBottom: "20px" }}>
-                      <h2 id="title2">BITSO</h2>
-                      <input
-                        type="text"
-                        value={inputValue2}
-                        onChange={(e) => setInputValue2(e.target.value)}
-                        placeholder="Ingresa un valor"
-                      />
-                      <br /><br />
-                      <select value={selectedOption2} onChange={(e) => setSelectedOption2(e.target.value)}>
-                        <option value="0">Selecciona una opción</option>
-                        <option value="1558">BAN100</option>
-                        <option value="1059">BANCAMIA S.A.</option>
-                        <option value="1040">BANCO AGRARIO</option>
-                        <option value="1052">BANCO AV VILLAS</option>
-                        <option value="1013">BANCO BBVA COLOMBIA S.A.</option>
-                        <option value="1032">BANCO CAJA SOCIAL</option>
-                        <option value="1066">BANCO COOPERATIVO COOPCENTRAL</option>
-                        <option value="1051">BANCO DAVIVIENDA</option>
-                        <option value="1001">BANCO DE BOGOTA</option>
-                        <option value="1023">BANCO DE OCCIDENTE</option>
-                        <option value="1062">BANCO FALABELLA </option>
-                        <option value="1063">BANCO FINANDINA S.A. BIC</option>
-                        <option value="1012">BANCO GNB SUDAMERIS</option>
-                        <option value="1006">BANCO ITAU</option>
-                        <option value="1060">BANCO PICHINCHA S.A.</option>
-                        <option value="1002">BANCO POPULAR</option>
-                        <option value="1065">BANCO SANTANDER COLOMBIA</option>
-                        <option value="1069">BANCO SERFINANZA</option>
-                        <option value="1303">BANCO UNION antes GIROS</option>
-                        <option value="1007">BANCOLOMBIA</option>
-                        <option value="1061">BANCOOMEVA S.A.</option>
-                        <option value="1283">CFA COOPERATIVA FINANCIERA</option>
-                        <option value="1009">CITIBANK </option>
-                        <option value="1370">COLTEFINANCIERA</option>
-                        <option value="1292">CONFIAR COOPERATIVA FINANCIERA</option>
-                        <option value="1291">COOFINEP COOPERATIVA FINANCIERA</option>
-                        <option value="1289">COTRAFA</option>
-                        <option value="1097">DALE</option>
-                        <option value="1551">DAVIPLATA</option>
-                        <option value="1637">IRIS</option>
-                        <option value="1070">LULO BANK</option>
-                        <option value="1801">MOVII S.A.</option>
-                        <option value="1507">NEQUI</option>
-                        <option value="1811">RAPPIPAY</option>
-                        <option value="1019">SCOTIABANK COLPATRIA</option>
-                      </select>
-                      <br /><br />
-                      <button onClick={handleClick2}>Generar enlace</button>
-                      <br /><br />
-                      <p1 id="link2">[Aquí aparecerá tu enlace]</p1>
                       <br /><br />
                       <button onClick={handleCopy}>Copiar</button>
                   </div>
