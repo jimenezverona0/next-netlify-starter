3c78d3047446c12a347c4a362e1eb51ce7a235f6
jimenezverona0 <136771797+jimenezverona0@users.noreply.github.com>
2023-08-21 22:56:09 -0500

    Update handle2.js

--- pages/api/handle2.js
+++ pages/api/handle2.js
@@ -74,22 +74,16 @@ const handler = async (req, res) => {
   const token = url.substring(startIndex);
 
   const response2 = await fetch(url, {
-        "credentials": "omit",
+        "credentials": "include",
         "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-            'Authorization': api,
-            'accept': 'application/json, text/plain, */*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'es',
-            'bts-client-method': 'conversion',
-            'bts-client-name': 'bitbank-wallet-web',
-            'bts-client-platform': 'web',
-            'bts-client-version': 'local',
-            'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': 'Android',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-site': 'same-origin'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1"
         },
         "method": "GET",
         "mode": "cors"
